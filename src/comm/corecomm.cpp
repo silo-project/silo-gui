f88a0ecbfddd4f192f972dc5653ad711dc4380e1
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-03-10 00:36:30 +0900

    Winsock2 And *nix

--- src/comm/corecomm.cpp
+++ src/comm/corecomm.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
-#include <sys/socket.h>
 
 #include "corecomm.hpp"
 
 using namespace std;
+
+
+#ifdef _WIN32
+#include <WinSock2.h>
+
+namespace CoreComm {
+
+	SOCKET sock;
+	WSADATA wsaData;
+
+	int connect() {
+		if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0) {
+			cout << "WSAStartup error " << WSAGetLastError() << endl;
+			WSACleanup();
+			return -1;
+		}
+
+		SOCKADDR_IN serv_addr;
+		serv_addr.sin_family = AF_INET; // address family Internet
+		serv_addr.sin_port = htons(COMM_PORT); //Port to connect on
+		serv_addr.sin_addr.s_addr = inet_addr(COMM_ADDR); //Target IP
+
+
+		sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Create socket
+		if (sock == INVALID_SOCKET)
+		{
+			cout << "socket() error : " << WSAGetLastError() << endl;
+			WSACleanup();
+			return -1;
+		}
+
+
+		if (connect(sock, reinterpret_cast<SOCKADDR *>(&serv_addr), sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
+		{
+			cout << "connect() error : " << WSAGetLastError() << endl;
+			WSACleanup();
+			return -1;
+		}
+		
+		return 0;
+	}
+
+	void disconnect() {
+		closesocket(sock);
+		WSACleanup();
+	}
+}
+
+#else
+#include <sys/socket.h>
 
 namespace CoreComm {
 
@@ -36,3 +85,4 @@ namespace CoreComm {
 		close(sockfd);
 	}
 }
+#endif
