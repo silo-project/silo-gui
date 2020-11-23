e7cc4a54db6ecafd916295a856fe54cdaf4a49fd
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-11-23 13:26:21 +0900

    좀 잘 동작하는 서버! cpp-httplib

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+
+
+#ifdef __WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+#include "httplib.h"
+
+
 int main(int argc, const char **argv) {
+    httplib::Server svr;
+
+    svr.set_mount_point("/", "./mount-origin");
+
+    svr.Get("/api", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 7681);
+
     return 0;
 }
