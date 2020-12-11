05dff58d5cf53faab0c7f287ed3b569a3d5f22c7
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-12-11 21:54:50 +0900

    part.cpp 부호 Warning 수정

--- src/part.cpp
+++ src/part.cpp
@@ -7,14 +7,18 @@
 cAbstractCircPart::cAbstractCircPart()       { this->type = ePartType::PartType_Circ; }
 cAbstractComputePart::cAbstractComputePart() { this->type = ePartType::PartType_Zip;  }
 
-position Position::fromLoc(const char * loc) {
-    return (atoi(loc + 1) << 32) | atoi(strchr(loc, ',') + 1);
+position Position::fromLoc(const char* loc) {
+    return (((uint64_t)atoi(loc + 1)) << 32u) | ((uint32_t)atoi(strchr(loc, ',') + 1));
+}
+
+position Position::fromPin(const char* pin) {
+    return (((uint64_t)atoi(pin)) << 32u) | ((uint32_t)atoi(strchr(pin, ',') + 1));
 }
 
 uint32_t Position::getX(position p) {
-    return (p >> 32) & 0x00000000FFFFFFFFul;
+    return (p >> 32u) & 0x00000000FFFFFFFFull;
 }
 
 uint32_t Position::getY(position p) {
-    return p & 0x00000000FFFFFFFFul;
+    return p & 0x00000000FFFFFFFFull;
 }
